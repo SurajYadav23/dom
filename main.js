c084adca923e66a95ffde747443e89f429912e18
suraj yadav <surajy9898@gmail.com>
2023-09-23 13:47:02 +0530

    save the json data in local storage

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+  const myForm = document.getElementById('myForm');
+
+  
+  function handleSubmit(event) {
+    event.preventDefault(); 
+    
+    
+    const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+      
+      const formdata = {
+          name: name,
+          email: email
+    }
+     
+      const formdataJson = JSON.stringify(formdata);
+
+      localStorage.setItem('form', formdataJson);
+    
+      console.log(name);
+      console.log(email);
+  }
+
+
+  myForm.addEventListener("submit", handleSubmit);
